0908f3720a09f944a3e62928565519cf69047eb0
Mendez Dev <mendezediel@gmail.com>
2023-02-15 13:14:09 -0600

    Add buildSrc Directory

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,42 @@
+/**
+ * To define plugins
+ */
+/*object BuildPlugins {
+    val android by lazy { "com.android.tools.build:gradle:${Versions.gradlePlugin}" }
+    val kotlin by lazy { "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}" }
+}*/
+
+/**
+ * To define dependencies
+ */
+object Dependencies {
+    //MAIN
+    val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val kotlin = "org.jetbrains.kotlin.android:${Versions.kotlin}"
+    val materialDesign = "com.google.android.material:material:${Versions.material}"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    val fragmentKtx = "androidx.fragment:fragment-ktx:${Versions.fragmentKtx}"
+    //LIFECYCLE
+    val lifecycle = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.androidxLifecycle}"
+    val lifecycleLivedata = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.androidxLifecycle}"
+    val lifecycleCoroutines = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.kotlinxCoroutines}"
+    //HTTP
+    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    val gson = "com.squareup.retrofit2:converter-gson:${Versions.gson}"
+    val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp3}"
+    //LOTTIE
+    val lottie = "com.airbnb.android:lottie:${Versions.lottie}"
+    //IMAGE
+    val picasso = "com.squareup.picasso:picasso:${Versions.picasso}"
+    //ROOM
+    val room = "androidx.room:room-runtime:${Versions.room}"
+    val roomKtx = "androidx.room:room-ktx:${Versions.room}"
+    val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
+
+
+    //TEST
+    val jUnit = "junit:junit:${Versions.jUnit}"
+    val testExt = "androidx.test.ext:junit:${Versions.androidTestRunner}"
+    val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espressoCore}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,25 @@
+object Versions {
+    val kotlin = "1.7.21"
+    val appCompat = "1.6.0"
+    val material = "1.7.0"
+    val coreKtx = "1.7.0"
+    val constraintLayout = "2.1.4"
+    val fragmentKtx = "1.4.1"
+    // TEST
+    val jUnit = "4.13.2"
+    val androidTestRunner = "1.1.5"
+    val espressoCore = "3.5.1"
+    // LIFECYCLE
+    val androidxLifecycle = "2.4.1"
+    val kotlinxCoroutines = "1.3.9"
+    //HTTP
+    val retrofit = "2.9.0"
+    val gson = "2.9.0"
+    val okhttp3 = "3.4.1"
+    //LOTTIE
+    val lottie = "5.0.3"
+    //IMAGE
+    val picasso = "2.8"
+    //ROOM
+    val room = "2.3.0"
+}
