ce80da8e012e6caf64de618066fec6980c3a8359
Mendez Dev <mendezediel@gmail.com>
2023-02-20 13:36:42 -0600

    Add Splash Screen

---
+++ app/src/main/java/com/ediel/mv/recipely/SplashScreenActivity.kt
@@ -0,0 +1,16 @@
+package com.ediel.mv.recipely
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
